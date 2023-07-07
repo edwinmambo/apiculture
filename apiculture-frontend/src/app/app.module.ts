bfb083f6ea7cc22645812bbb66449283c6d8d81e
Edwin Mambo <edwinmambo33@gmail.com>
2023-07-07 16:56:58 +0300

    feat: Introduced Footer/Navbar
    - Added a footer with a navbar containing proposed pages

--- apiculture-frontend/src/app/app.module.ts
+++ apiculture-frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
