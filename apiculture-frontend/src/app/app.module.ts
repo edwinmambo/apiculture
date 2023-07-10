00aef1c0d266aa860d548d7554f17acf5bc486d8
Edwin Mambo <edwinmambo33@gmail.com>
2023-07-10 18:50:43 -0400

    feat: Homepage Main Section
    - Added the homepage main section
    - Added the banner image with its resource asset

--- apiculture-frontend/src/app/app.module.ts
+++ apiculture-frontend/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
